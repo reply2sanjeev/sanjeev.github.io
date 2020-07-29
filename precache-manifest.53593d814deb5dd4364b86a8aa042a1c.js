f4e87f013d87f551a0aedbe506fe5c45098a8a9e
reply2sanjeev <reply2sanjeev@live.com>
2020-07-29 17:59:10 +0530

    hi
    hi

---
+++ precache-manifest.53593d814deb5dd4364b86a8aa042a1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699074540986ac87bcbfbcadb08164bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "22de31cdff0eaabcb6f6",
+    "url": "/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22de31cdff0eaabcb6f6",
+    "url": "/static/js/main.ad8db6ca.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "007ddacac05f9fd6abee938d2ff0afc0",
+    "url": "/static/media/resume.007ddaca.pdf"
+  }
+]);
